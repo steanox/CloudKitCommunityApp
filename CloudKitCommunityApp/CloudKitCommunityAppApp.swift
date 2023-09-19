763b310508b52460f0d2143ab0995913d1aef777
Octavianus <oktagandajaya@gmail.com>
2023-09-19 16:01:21 +0700

    Initial Commit

---
+++ CloudKitCommunityApp/CloudKitCommunityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitCommunityAppApp.swift
+//  CloudKitCommunityApp
+//
+//  Created by octavianus on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitCommunityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
